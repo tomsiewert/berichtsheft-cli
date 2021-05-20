4c60eb1f38097f308e5a43e4ca94089d1e44d105
Tom Siewert <tom@siewert.io>
2021-05-20 09:07:39 +0200

    db/migrations: Do not create a separate table for days
    Signed-off-by: Tom Siewert <tom@siewert.io>

--- db/migrations/000001_create_days_table.up.sql
+++ db/migrations/000001_create_days_table.up.sql
@@ -1,15 +1,7 @@
-CREATE TABLE IF NOT EXISTS days
-(
-    daysID int auto_increment NOT NULL,
-    daysDate DATE NOT NULL,
-    PRIMARY KEY (daysID)
-);
-
 CREATE TABLE IF NOT EXISTS tasks
 (
     taskID int auto_increment NOT NULL,
-    dayID int,
+    taskDate DATE NOT NULL,
     taskDescription VARCHAR,
-    PRIMARY KEY (taskID),
-    FOREIGN KEY (dayID) REFERENCES days(daysID)
+    PRIMARY KEY (taskID)
 );
