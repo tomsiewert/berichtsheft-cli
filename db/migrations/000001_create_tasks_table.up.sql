1b0fae2e91b978a4ea284c9dd7be734cc35265ad
Tom Siewert <tom@siewert.io>
2021-05-20 19:22:02 +0200

    db/migrations: move migrations file name

--- db/migrations/000001_create_tasks_table.up.sql
+++ db/migrations/000001_create_tasks_table.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS tasks
 (
     taskID int auto_increment NOT NULL,
     taskDate DATE NOT NULL,
+    taskTime BIGINT(8) NOT NULL,
     taskDescription VARCHAR,
     PRIMARY KEY (taskID)
 );
